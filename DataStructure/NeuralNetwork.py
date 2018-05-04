3b5d70b21e8c79c84055334190d806ad5af3331f
Krzysztof Barden <202670@edu.p.lodz.pl>
2018-05-04 22:43:06 +0200

    Starting

--- DataStructure/NeuralNetwork.py
+++ DataStructure/NeuralNetwork.py
@@ -1,10 +1,15 @@
+import numpy
+import library
+
 
 class NeuralNetwork:
-    def __init__(self, inputnodes, hiddennodes, outputnodes, learningrate, momentum):
-        self.input_nodes = inputnodes
-        self.hidden_nodes = hiddennodes
-        self.output_nodes = outputnodes
-        # learning rate
-        self.learning_rate = learningrate
+    def __init__(self, input_nodes, hidden_nodes, output_nodes, learning_rate, momentum):
+        self.input_nodes = input_nodes
+        self.hidden_nodes = hidden_nodes
+        self.output_nodes = output_nodes
+        self.learning_rate = learning_rate
         self.momentum = momentum
-        pass
+
+        self.wih = (numpy.random.rand(self.hidden_nodes, self.input_nodes) - 0.5)  # weight input to hidden
+        self.who = (numpy.random.rand(self.output_nodes, self.hidden_nodes) - 0.5)  # weight hidden to output
+        self.activation_function = lambda x: library.sigmoid(x)
