89640c2660a2692e5dbdddf2499994337e7767ce
Krzysztof Barden <202670@edu.p.lodz.pl>
2018-05-05 16:16:55 +0200

    query neural network

--- DataStructure/NeuralNetwork.py
+++ DataStructure/NeuralNetwork.py
@@ -13,3 +13,18 @@ class NeuralNetwork:
         self.wih = (numpy.random.rand(self.hidden_nodes, self.input_nodes) - 0.5)  # weight input to hidden
         self.who = (numpy.random.rand(self.output_nodes, self.hidden_nodes) - 0.5)  # weight hidden to output
         self.activation_function = lambda x: library.sigmoid(x)
+
+    def train(self):
+        return
+
+    def query(self, input_list):
+        inputs = numpy.array(input_list, ndmin=2).T
+
+        hidden_inputs = numpy.dot(self.wih, inputs)
+        hidden_outputs = self.activation_function(hidden_inputs)
+
+        final_outputs = numpy.dot(self.who, hidden_outputs)
+
+        final_outputs = self.activation_function(final_outputs)
+
+        return final_outputs
--- library.py
+++ library.py
@@ -1,7 +1,9 @@
 import numpy as np
 
+
 def sigmoid(x):
-    return 1/(1+np.exp(-x))
+    return 1 / (1 + np.exp(-x))
+
 
 def sigmoid_der(x):
-    return x*(1-x)
+    return x * (1 - x)
