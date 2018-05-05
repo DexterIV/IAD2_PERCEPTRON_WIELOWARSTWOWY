b6066af0cd6cc99ec1fa04ac2fcbbbe4a9809855
Krzysztof Barden <202670@edu.p.lodz.pl>
2018-05-05 23:12:04 +0200

    testing if it works

--- DataStructure/NeuralNetwork.py
+++ DataStructure/NeuralNetwork.py
@@ -38,6 +38,28 @@ class NeuralNetwork:
             pass
         pass
 
+    def train_manual_epochs(self, input_list, target_list):
+        # convert inputs list to 2d array
+        inputs = numpy.array(input_list, ndmin=2).T
+        targets = numpy.array(target_list, ndmin=2).T
+        # calculate signals into hidden layer
+        hidden_inputs = numpy.dot(self.wih, inputs)
+        # calculate the signals emerging from hidden layer
+        hidden_outputs = self.activation_function(hidden_inputs)
+        # calculate signals into final output layer
+        final_inputs = numpy.dot(self.who, hidden_outputs)
+
+        # calculate the signals emerging from final output layer
+        final_outputs = self.activation_function(final_inputs)
+        # output layer error i)­ actual)
+        output_errors = targets - final_outputs
+        hidden_errors = numpy.dot(self.who.T, output_errors)
+        self.who += self.learning_rate * numpy.dot((output_errors * final_outputs * (1.0 - final_outputs)),
+                                                       numpy.transpose(hidden_outputs))
+        self.wih += self.learning_rate * numpy.dot((hidden_errors * hidden_outputs * (1.0 - hidden_outputs)),
+                                                       numpy.transpose(inputs))
+        pass
+
     def query(self, input_list):
         inputs = numpy.array(input_list, ndmin=2).T  # convert inputs list to 2d array
 
--- main.py
+++ main.py
@@ -1,14 +1,25 @@
+import random
+import decimal
+
+import numpy
+
 from DataStructure.NeuralNetwork import NeuralNetwork
 
-network = NeuralNetwork(4, 2, 4, 0.1, 0)
 
-input = [0, 1, 0, 0]
+'''
+network = NeuralNetwork(4, 2, 4, 0.6, 0)
 
-output = [0, 1, 0, 0]
+input_list = [1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]
+output = [1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]
+epochs = 10000
 
-network.train(input, output, 1000)
-
-fin = network.query(input)
-print(input)
+for e in range(3):
+    network.train(input_list[e], output[e], 10000)
+numpy.set_printoptions(suppress=True)  # avoid e-05 notation
+fin = [network.query(input_list[0]),
+       network.query(input_list[1]),
+       network.query(input_list[2]),
+       network.query(input_list[3])]
 print(output)
 print(fin)
+'''
