7e8088aab8dca56ea9350d1610d70a4f03443f45
Krzysztof Barden <202670@edu.p.lodz.pl>
2018-05-06 22:23:58 +0200

    todo bias momentum online offline

---
+++ Testing/cwiczenie2.py
@@ -0,0 +1,34 @@
+import random
+import numpy
+
+from DataStructure.NeuralNetwork import NeuralNetwork
+
+
+def task_2():
+    network = NeuralNetwork(4, 6, 4, 0.1, 0)
+
+    input_list = [1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]
+    output = [1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]
+    epochs = 10000
+    x = numpy.zeros(epochs * 4, dtype=int)
+
+    for i in range(epochs):
+        x[i] = 0
+        x[i + epochs] = 1
+        x[i + epochs * 2] = 2
+        x[i + epochs * 3] = 3
+
+    random.shuffle(x)
+
+    for e in range(len(x)):
+        network.train_manual_epochs(input_list[(x[e])], output[(x[e])])
+
+    numpy.set_printoptions(suppress=True)  # avoid e-05 notation
+    fin = [network.query(input_list[0]),
+           network.query(input_list[1]),
+           network.query(input_list[2]),
+           network.query(input_list[3])]
+    print(output)
+
+    for elem in range(len(fin)):
+        print(fin[elem])
--- main.py
+++ main.py
@@ -1,25 +1,3 @@
-import random
-import decimal
+from Testing.cwiczenie2 import task_2
 
-import numpy
-
-from DataStructure.NeuralNetwork import NeuralNetwork
-
-
-'''
-network = NeuralNetwork(4, 2, 4, 0.6, 0)
-
-input_list = [1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]
-output = [1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]
-epochs = 10000
-
-for e in range(3):
-    network.train(input_list[e], output[e], 10000)
-numpy.set_printoptions(suppress=True)  # avoid e-05 notation
-fin = [network.query(input_list[0]),
-       network.query(input_list[1]),
-       network.query(input_list[2]),
-       network.query(input_list[3])]
-print(output)
-print(fin)
-'''
+#task_2()
