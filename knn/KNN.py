38f730200a695db3f69dd6f0c4494fe8015fde64
Krzysztof Barden <wiedzmenpl@gmail.com>
2018-05-22 23:34:48 +0200

    minor tweaks

--- knn/KNN.py
+++ knn/KNN.py
@@ -2,7 +2,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.metrics import classification_report
 
 
 def task_KNN():
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import random
 
-from Testing import *
+from Testing.Iris import task_Iris
+from Testing.Seeds import task_Seeds
+from Testing.Task2 import task_2
 from knn.KNN import task_KNN
 
 # task_2()
-# task_Iris()
+task_Iris()
 # task_Seeds()
-task_KNN()
+# task_KNN()
